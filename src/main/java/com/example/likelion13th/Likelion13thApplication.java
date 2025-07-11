3bedc622bcef46c1482a052e5aef406554762948
luckylee524 <dbleecomkr@naver.com>
2025-07-11 21:26:46 +0900

    week15 healthcheck API

---
+++ src/main/java/com/example/likelion13th/Likelion13thApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion13th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion13thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion13thApplication.class, args);
+	}
+
+}

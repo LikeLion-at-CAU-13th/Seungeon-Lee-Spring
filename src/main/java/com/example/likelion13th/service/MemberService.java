b650c676efe0798d7f4514f59cc47ffd24a7ca96
leeseungeon <dbleecomkr@naver.com>
2025-11-05 21:07:58 +0900

    remove: 중복코드 삭제

--- src/main/java/com/example/likelion13th/service/MemberService.java
+++ src/main/java/com/example/likelion13th/service/MemberService.java
@@ -5,6 +5,10 @@ import com.example.likelion13th.dto.request.JoinRequestDto;
 import com.example.likelion13th.exception.MemberAlreadyExistsException;
 import com.example.likelion13th.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,23 +23,6 @@ public class MemberService {
     private final MemberRepository memberRepository;
     // 비밀번호 인코더 DI(생성자 주입)
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    // 비밀번호 인코더 DI(생성자 주입)
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public void join(JoinRequestDto joinRequestDto) {
-        // 해당 name이 이미 존재하는 경우
-        if (memberRepository.existsByName(joinRequestDto.getName())){
-            throw new MemberAlreadyExistsException("이미 존재하는 사용자 입니다.");
-        }
-
-        // 유저 객체 생성
-        Member member = joinRequestDto.toEntity(bCryptPasswordEncoder);
-
-        // 유저 정보 저장
-        memberRepository.save(member);
-    }
-
 
     public Page<Member> getMembersByPage(int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
